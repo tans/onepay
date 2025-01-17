f1e8385c05385b989907a58a364977e800569ba2
tans <tans@imac.local>
2025-01-17 13:55:53 +0800

    -

--- src/pages/api/query-order.ts
+++ src/pages/api/query-order.ts
@@ -1,7 +1,11 @@
 import db from "@/lib/db";
 
+export const prerender = false;
 export const GET = async (req: Request) => {
-    const { outTradeNo } = req.query;
+    const outTradeNo = new URL(req.url).searchParams.get("outTradeNo");
+    if (!outTradeNo) {
+        return new Response(JSON.stringify({ code: 400, message: "outTradeNo 不能为空" }));
+    }
     const order = await db.onepay.findOne({ outTradeNo });
 
     if (!order) {
