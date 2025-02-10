5324c3d8c6dc5cec24536a17a5d5a50a150e4a8e
tans <tans@imac.local>
2025-02-10 17:45:25 +0800

    -

--- src/pages/api/[id]/alipay-notify.ts
+++ src/pages/api/[id]/alipay-notify.ts
@@ -20,3 +20,22 @@ export const GET: APIRoute = async ({ request }) => {
 
     return Response.json({ success: true })
 }
+
+export const POST: APIRoute = async ({ request }) => {
+    const formData = await request.formData()
+    const params = Object.fromEntries(formData)
+    console.log(params)
+
+    let order = await db.onepay.findOne({ _id: new ObjectId(params.out_trade_no) })
+    if (!order) {
+        return Response.json({ success: false, message: "订单不存在" })
+    }
+
+    if (order.status === 'paid') {
+        return Response.json({ success: false, message: "订单已支付" })
+    }
+
+    await db.onepay.updateOne({ _id: order._id }, { $set: { status: 'paid' } })
+
+    return Response.json({ success: true })
+}
